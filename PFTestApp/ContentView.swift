017b0181c155129484a19dc66c0ac27ef5e5e648
Misak <azurovymisak@outlook.com>
2024-01-26 00:48:54 -0800

    Initial Commit

---
+++ PFTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PFTestApp
+//
+//  Created by Sysprobs on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PFTestApp/PFTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PFTestAppApp.swift
+//  PFTestApp
+//
+//  Created by Sysprobs on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PFTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
